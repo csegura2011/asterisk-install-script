4487dce4fbd252a303c88ee2a7be1ad0db6ecaf6
Phillip Whelan <pwhelan@timestamp.cl>
2016-08-27 22:06:27 -0300

    creating first version of the script

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 #asterisk-install-script
+
+Asterisk IP PBX installation script
+
+## What is Asterisk?
+
+Asterisk is a software implementation of Private Branch Exchange (PBX) 
+
---
+++ install_asterisk_11_on_centos6_i386.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+###############################################################################
+#:File:install_asterisk_11_on_centos6_i386.sh
+#:Author:Cristian Segura Lepe <cristian.segura.lepe@gmail.com>
+#:Creation Date:
+###############################################################################
+
+
+# Install this script dependencies 
+yum -y install wget awk sed
+
+# Disable SELinux using sed
+
+
+
+
+
