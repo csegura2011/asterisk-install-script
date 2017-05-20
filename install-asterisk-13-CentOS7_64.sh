47e0111af6c14f573a2e44c4ec130e3973e1eb48
root <root@localhost.localdomain>
2017-05-20 08:18:11 -0400

    Asterisk 13 Installation over CentOS 7

--- install-asterisk-13-CentOS7_64.sh
+++ install-asterisk-13-CentOS7_64.sh
@@ -12,6 +12,24 @@
 #                                                                                        #
 ##########################################################################################
 
+# Variables
+
+tstamp=$(date +%Y%m%d%H%M%S)
+
+ast_url="http://downloads.asterisk.org/pub/telephony/asterisk"
+ast_tarfile="asterisk-13-current.tar.gz"
+dahdi_url="http://downloads.asterisk.org/pub/telephony/dahdi-linux-complete"
+dahdi_tarfile="dahdi-linux-complete-current.tar.gz"
+libpri_url="http://downloads.asterisk.org/pub/telephony/libpri"
+libpri_tarfile="libpri-current.tar.gz"
+
+workdir="${tstamp}_asterisk_downloads"
+
+
+#-----------------------------------------------------------------------------#
+# Install dependencies                                                        #
+#-----------------------------------------------------------------------------#
+
 # Install Utilities (optional)
 yum -y install vim  # advanced text-editor
 yum -y sed          # stream editor 
@@ -19,5 +37,23 @@ yum -y awk          # per-line editor
 
 # Install this script dependencies 
 yum -y install wget  
+
+# Create a new working directory
+mkdir $workdir 
+
+#-----------------------------------------------------------------------------#
+# Download source code                                                        #
+#-----------------------------------------------------------------------------#
+
+cd $workdir
+# Download asterisk source code
+wget ${ast_url}/${ast_tarfile}
+wget ${dahdi_url}/${dahdi_tarfile}
+wget ${libpri_url}/${libpri_tarfile}
+
+tar -xzvf ${ast_tarfile}
+tar -xzvf ${dahdi_tarfile}
+tar -xzvf ${libpri_tarfile}
+
 
 
